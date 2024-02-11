ad3ed4fa272b9f2faa5a386f4a861b8917b5150a
AbhishekASLK <bhoslea925@gmail.com>
2024-02-11 23:36:46 +0530

    improved the code

--- lib/quiz_app.dart
+++ lib/quiz_app.dart
@@ -8,64 +8,82 @@ class QuizApp extends StatefulWidget {
 }
 
 class _QuizAppState extends State<QuizApp> {
+  bool questionScreen = true;
+  int questionIndex = 0;
+  int selectedIndex = -1;
+  bool flag1 = false;
+  bool flag2 = false;
+  bool flag3 = false;
+  bool flag4 = false;
+  int score = 0;
+
   void reset() {
     flag1 = false;
     flag2 = false;
     flag3 = false;
     flag4 = false;
-    redFlag1 = false;
-    redFlag2 = false;
-    redFlag3 = false;
-    redFlag4 = false;
-    isAttempted = false;
+    selectedIndex = -1;
   }
 
-  int score = 0;
-  bool questionScreen = true;
-  int questionIndex = 0;
-  int selectedOption = -1;
-  bool flag1 = false;
-  bool flag2 = false;
-  bool flag3 = false;
-  bool flag4 = false;
-  bool redFlag1 = false;
-  bool redFlag2 = false;
-  bool redFlag3 = false;
-  bool redFlag4 = false;
-  bool isAttempted = false;
   List<Map> allQuestions = [
     {
-      "question": "Who is founder of Apple?",
-      "options": ["Steve Jobs", "Elon Musk", "Jeff Bezos", "Larry Page"],
+      "question": "What is Unix?",
+      "options": [
+        "Programming Language",
+        "Operating System",
+        "Text Editor",
+        "Kernel"
+      ],
+      "answerIndex": 1,
+    },
+    {
+      "question": "In which language UNIX is written?",
+      "options": [
+        "C",
+        "C++",
+        "Java",
+        "Python",
+      ],
       "answerIndex": 0,
     },
     {
-      "question": "Who is founder of Amazon?",
-      "options": ["Steve Jobs", "Elon Musk", "Jeff Bezos", "Larry Page"],
-      "answerIndex": 2,
-    },
-    {
-      "question": "Who is founder of Google?",
-      "options": ["Steve Jobs", "Elon Musk", "Jeff Bezos", "Larry Page"],
+      "question": "Which of the following is the first UNIX editor?",
+      "options": [
+        "vi",
+        "emacs",
+        "ex",
+        "ed",
+      ],
       "answerIndex": 3,
     },
     {
-      "question": "Who is founder of SpaceX?",
-      "options": ["Steve Jobs", "Elon Musk", "Jeff Bezos", "Larry Page"],
+      "question": "Which of the following is not a feature of Unix?",
+      "options": [
+        "multiuser",
+        "easy to use",
+        "multitasking",
+        "portability",
+      ],
       "answerIndex": 1,
     },
     {
-      "question": "Who is founder of Facebook?",
-      "options": ["Steve Jobs", "Mark Zuckerberg", "Jeff Bezos", "Larry Page"],
-      "answerIndex": 1,
+      "question": "Which editor is used by the Unix system to edit files?",
+      "options": [
+        "word",
+        "notepad++ ",
+        "vi",
+        "notepad",
+      ],
+      "answerIndex": 2,
     },
   ];
 
   Scaffold isQuestionScreen() {
     if (questionScreen) {
       return Scaffold(
+        backgroundColor: Colors.pink,
         appBar: AppBar(
-          backgroundColor: Colors.red,
+          backgroundColor: Colors.orange,
           title: const Text('Quiz App'),
           centerTitle: true,
         ),
@@ -102,39 +120,27 @@ class _QuizAppState extends State<QuizApp> {
             ElevatedButton(
               onPressed: () {
                 setState(() {
-                  isAttempted = true;
-                  selectedOption = 0;
-                  if (selectedOption ==
-                      allQuestions[questionIndex]['answerIndex']) {
+                  selectedIndex = 0;
+                  if (allQuestions[questionIndex]['answerIndex'] == 0) {
                     flag1 = true;
-                    score++;
-                  } else {
-                    redFlag1 = true;
-                    if (allQuestions[questionIndex]['answerIndex'] == 0) {
-                      flag1 = true;
-                    }
-                    if (allQuestions[questionIndex]['answerIndex'] == 1) {
-                      flag2 = true;
-                    }
-                    if (allQuestions[questionIndex]['answerIndex'] == 2) {
-                      flag3 = true;
-                    }
-                    if (allQuestions[questionIndex]['answerIndex'] == 3) {
-                      flag4 = true;
-                    }
                   }
                 });
               },
-              style: (flag1)
-                  ? const ButtonStyle(
-                      backgroundColor: MaterialStatePropertyAll(Colors.green))
-                  : (redFlag1)
-                      ? const ButtonStyle(
-                          backgroundColor: MaterialStatePropertyAll(Colors.red))
-                      : const ButtonStyle(
-                          backgroundColor:
-                              MaterialStatePropertyAll(Colors.blue),
-                        ),
+              style: ButtonStyle(
+                  fixedSize: const MaterialStatePropertyAll(
+                    Size(300, 50),
+                  ),
+                  backgroundColor: (flag1 ||
+                          (selectedIndex != -1 &&
+                              allQuestions[questionIndex]['answerIndex'] == 0))
+                      ? const MaterialStatePropertyAll(Colors.green)
+                      : (selectedIndex == 0)
+                          ? const MaterialStatePropertyAll(
+                              Colors.red,
+                            )
+                          : const MaterialStatePropertyAll(
+                              Colors.blue,
+                            )),
               child: Text(
                 allQuestions[questionIndex]['options'][0],
                 style: const TextStyle(
@@ -143,46 +149,35 @@ class _QuizAppState extends State<QuizApp> {
               ),
             ),
             const SizedBox(
-              height: 10,
+              height: 25,
             ),
 
             // ================= Option 2 ===========================
             ElevatedButton(
               onPressed: () {
                 setState(() {
-                  isAttempted = true;
-                  selectedOption = 1;
-                  if (selectedOption ==
-                      allQuestions[questionIndex]['answerIndex']) {
+                  selectedIndex = 1;
+                  if (allQuestions[questionIndex]['answerIndex'] == 1) {
                     flag2 = true;
-                    score++;
-                  } else {
-                    redFlag2 = true;
-                    if (allQuestions[questionIndex]['answerIndex'] == 0) {
-                      flag1 = true;
-                    }
-                    if (allQuestions[questionIndex]['answerIndex'] == 1) {
-                      flag2 = true;
-                    }
-                    if (allQuestions[questionIndex]['answerIndex'] == 2) {
-                      flag3 = true;
-                    }
-                    if (allQuestions[questionIndex]['answerIndex'] == 3) {
-                      flag4 = true;
-                    }
                   }
                 });
               },
-              style: (flag2)
-                  ? const ButtonStyle(
-                      backgroundColor: MaterialStatePropertyAll(Colors.green))
-                  : (redFlag2)
-                      ? const ButtonStyle(
-                          backgroundColor: MaterialStatePropertyAll(Colors.red))
-                      : const ButtonStyle(
-                          backgroundColor:
-                              MaterialStatePropertyAll(Colors.blue),
-                        ),
+              style: ButtonStyle(
+                fixedSize: const MaterialStatePropertyAll(
+                  Size(300, 50),
+                ),
+                backgroundColor: (flag2 ||
+                        (selectedIndex != -1 &&
+                            allQuestions[questionIndex]['answerIndex'] == 1))
+                    ? const MaterialStatePropertyAll(Colors.green)
+                    : (selectedIndex == 1)
+                        ? const MaterialStatePropertyAll(
+                            Colors.red,
+                          )
+                        : const MaterialStatePropertyAll(
+                            Colors.blue,
+                          ),
+              ),
               child: Text(
                 allQuestions[questionIndex]['options'][1],
                 style: const TextStyle(
@@ -191,46 +186,35 @@ class _QuizAppState extends State<QuizApp> {
               ),
             ),
             const SizedBox(
-              height: 10,
+              height: 25,
             ),
 
             // ================= Option 3 ===========================
             ElevatedButton(
               onPressed: () {
                 setState(() {
-                  isAttempted = true;
-                  selectedOption = 2;
-                  if (selectedOption ==
-                      allQuestions[questionIndex]['answerIndex']) {
+                  selectedIndex = 2;
+                  if (allQuestions[questionIndex]['answerIndex'] == 2) {
                     flag3 = true;
-                    score++;
-                  } else {
-                    redFlag3 = true;
-                    if (allQuestions[questionIndex]['answerIndex'] == 0) {
-                      flag1 = true;
-                    }
-                    if (allQuestions[questionIndex]['answerIndex'] == 1) {
-                      flag2 = true;
-                    }
-                    if (allQuestions[questionIndex]['answerIndex'] == 2) {
-                      flag3 = true;
-                    }
-                    if (allQuestions[questionIndex]['answerIndex'] == 3) {
-                      flag4 = true;
-                    }
                   }
                 });
               },
-              style: (flag3)
-                  ? const ButtonStyle(
-                      backgroundColor: MaterialStatePropertyAll(Colors.green))
-                  : (redFlag3)
-                      ? const ButtonStyle(
-                          backgroundColor: MaterialStatePropertyAll(Colors.red))
-                      : const ButtonStyle(
-                          backgroundColor:
-                              MaterialStatePropertyAll(Colors.blue),
-                        ),
+              style: ButtonStyle(
+                fixedSize: const MaterialStatePropertyAll(
+                  Size(300, 50),
+                ),
+                backgroundColor: (flag3 ||
+                        (selectedIndex != -1 &&
+                            allQuestions[questionIndex]['answerIndex'] == 2))
+                    ? const MaterialStatePropertyAll(Colors.green)
+                    : (selectedIndex == 2)
+                        ? const MaterialStatePropertyAll(
+                            Colors.red,
+                          )
+                        : const MaterialStatePropertyAll(
+                            Colors.blue,
+                          ),
+              ),
               child: Text(
                 allQuestions[questionIndex]['options'][2],
                 style: const TextStyle(
@@ -239,46 +223,35 @@ class _QuizAppState extends State<QuizApp> {
               ),
             ),
             const SizedBox(
-              height: 10,
+              height: 25,
             ),
 
             // ================= Option 4 ===========================
             ElevatedButton(
               onPressed: () {
                 setState(() {
-                  isAttempted = true;
-                  selectedOption = 3;
-                  if (selectedOption ==
-                      allQuestions[questionIndex]['answerIndex']) {
+                  selectedIndex = 3;
+                  if (allQuestions[questionIndex]['answerIndex'] == 3) {
                     flag4 = true;
-                    score++;
-                  } else {
-                    redFlag4 = true;
-                    if (allQuestions[questionIndex]['answerIndex'] == 0) {
-                      flag1 = true;
-                    }
-                    if (allQuestions[questionIndex]['answerIndex'] == 1) {
-                      flag2 = true;
-                    }
-                    if (allQuestions[questionIndex]['answerIndex'] == 2) {
-                      flag3 = true;
-                    }
-                    if (allQuestions[questionIndex]['answerIndex'] == 3) {
-                      flag4 = true;
-                    }
                   }
                 });
               },
-              style: (flag4)
-                  ? const ButtonStyle(
-                      backgroundColor: MaterialStatePropertyAll(Colors.green))
-                  : (redFlag4)
-                      ? const ButtonStyle(
-                          backgroundColor: MaterialStatePropertyAll(Colors.red))
-                      : const ButtonStyle(
-                          backgroundColor:
-                              MaterialStatePropertyAll(Colors.blue),
-                        ),
+              style: ButtonStyle(
+                fixedSize: const MaterialStatePropertyAll(
+                  Size(300, 50),
+                ),
+                backgroundColor: (flag4 ||
+                        (selectedIndex != -1 &&
+                            allQuestions[questionIndex]['answerIndex'] == 3))
+                    ? const MaterialStatePropertyAll(Colors.green)
+                    : (selectedIndex == 3)
+                        ? const MaterialStatePropertyAll(
+                            Colors.red,
+                          )
+                        : const MaterialStatePropertyAll(
+                            Colors.blue,
+                          ),
+              ),
               child: Text(
                 allQuestions[questionIndex]['options'][3],
                 style: const TextStyle(
@@ -291,15 +264,14 @@ class _QuizAppState extends State<QuizApp> {
         floatingActionButton: FloatingActionButton(
           onPressed: () {
             setState(() {
-              if (allQuestions.length != questionIndex && isAttempted) {
+              if (questionIndex < allQuestions.length && selectedIndex != -1) {
                 questionIndex++;
-                if (questionIndex == allQuestions.length) {
-                  questionIndex = 0;
-                  questionScreen = false;
-                }
+                reset();
+              }
+              if (questionIndex == allQuestions.length) {
+                questionScreen = false;
               }
             });
-            reset();
           },
           backgroundColor: Colors.amber,
           child: const Icon(
@@ -310,33 +282,45 @@ class _QuizAppState extends State<QuizApp> {
       );
     } else {
       return Scaffold(
+        backgroundColor: Colors.pink,
         appBar: AppBar(
-          title: const Text('Result'),
-          backgroundColor: Colors.blue,
+          title: const Text(
+            'Result',
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+          backgroundColor: Colors.orange,
         ),
         body: Center(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
+              Text(
+                '$score/${allQuestions.length}',
+                style: const TextStyle(
+                  fontSize: 30,
+                  color: Colors.white,
+                ),
+              ),
               const SizedBox(
-                height: 20,
+                height: 10,
               ),
               const Text(
                 'Score',
                 style: TextStyle(
-                  fontSize: 30,
+                  fontSize: 20,
+                  color: Colors.white,
                 ),
               ),
-              Text(
-                '$score/${allQuestions.length}',
-                style: const TextStyle(
-                  fontSize: 20,
-                ),
+              const SizedBox(
+                height: 10,
               ),
               ElevatedButton(
                   onPressed: () {
                     setState(() {
+                      questionIndex = 0;
                       questionScreen = true;
-                      score = 0;
                     });
                   },
                   child: const Text('Reset'))
